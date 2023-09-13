432fcf3510c7bc8fc1a8f3c4a0ef0692b36a368d
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-09-13 19:11:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ export default function Home() {
               <Button colorScheme="blue" mr={3} onClick={onClose}>
                 Close
               </Button>
-              <Button variant="ghost">Register NOW</Button>
+
             </ModalFooter>
           </ModalContent>
         </Modal>
