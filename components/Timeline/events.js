eadeb87c874a044dcf057980814c345bba915568
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-09-14 15:58:06 +0530

    Update events.js

--- components/Timeline/events.js
+++ components/Timeline/events.js
@@ -183,10 +183,10 @@ const events = [
     title: "CODE-IT-ON",
     desc: "Code It On is an exciting codeathon event where participants from diverse backgrounds and skill levels come together to showcase their coding prowess. 
 This competition challenges participants to solve real-world problems through creative and innovative coding solutions. Whether you're a seasoned developer or just starting your coding journey, Code It On offers a platform to learn, collaborate, and compete in a friendly and supportive environment. 
-Join us for a coding adventure where you can test your skills, network with fellow enthusiasts, and win exciting prizes!",
+Join usfor a coding adventure where you can test your skills, networkwith fellow enthusiasts, and win exciting prizes!",
     s_desc:"Code It On is an exciting codeathon event where participants from diverse backgrounds and skill levels come together to showcase their coding prowess. 
 This competition challenges participants to solve real-world problems through creative and innovative coding solutions. Whether you're a seasoned developer or just starting your coding journey, Code It On offers a platform to learn, collaborate, and compete in a friendly and supportive environment. 
-Join us for a coding adventure where you can test your skills, network with fellow enthusiasts, and win exciting prizes!",
+Join usfor a coding adventure where you can test your skills, networkwith fellow enthusiasts, and win exciting prizes!",
     dates: "22nd sept",
     time: "11am Onwards",
     team_size: "1",
