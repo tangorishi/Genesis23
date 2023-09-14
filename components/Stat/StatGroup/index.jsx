17e685fdd678782ef049f130eb3d8c7db487ea55
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-09-14 15:09:01 +0530

    Update index.jsx

--- components/Stat/StatGroup/index.jsx
+++ components/Stat/StatGroup/index.jsx
@@ -12,7 +12,7 @@ const stats = [
     statistic_subject: "Sponsors",
   },
   {
-    statistic_number: "22nd-25th september",
+    statistic_number: "22nd-25th sept",
     statistic_subject: "Dates",
   },
   {
