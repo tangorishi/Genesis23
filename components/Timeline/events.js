a1c8e0fb2b6eaaccc4000206c8d22734dd0f91bf
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-09-14 15:48:15 +0530

    Update events.js

--- components/Timeline/events.js
+++ components/Timeline/events.js
@@ -181,14 +181,18 @@ const events = [
     image:
       "https://images.prismic.io/ieeemuj/d9eedf50-249b-4404-967b-25c48494cf28_code_poster.png?auto=compress,format",
     title: "CODE-IT-ON",
-    desc: "Lights, Camera, Auction! Is where you can prove that you’re armed with a creative and strategic mind. It is a bid, buy, and sell competition, where the participants first place bids on their choice of items, and then try to find creative ways to sell said items to the judges.Lights, Camera, Auction! Allows participants into a unique blend of circumstances designed to stress-test their creativity.",
-    s_desc:"Lights, Camera, Auction! Is where you can prove that you’re armed with a creative and strategic mind. It is a bid, buy, and sell competition, where the participants first place bids on their choice of items, and then try to find creative ways to sell said items to the judges.Lights, Camera, Auction! Allows participants into a unique blend of circumstances designed to stress-test their creativity.",
-    dates: "22nd sept to 23rd sept",
-    time: "11pm to 6am",
-    team_size: "3",
-    prize: "Rs. 25,000",
-    form: "https://forms.gle/J6WUmXqvm6bhqd8r8",
-    venue: "Campus",
+    desc: "Code It On is an exciting codeathon event where participants from diverse backgrounds and skill levels come together to showcase their coding prowess. 💫
+👩‍💻This competition challenges participants to solve real-world problems through creative and innovative coding solutions. Whether you're a seasoned developer or just starting your coding journey, Code It On offers a platform to learn, collaborate, and compete in a friendly and supportive environment. 💻 🌐
+Join us for a coding adventure where you can test your skills, network with fellow enthusiasts, and win exciting prizes! 👫✨",
+    s_desc:"Code It On is an exciting codeathon event where participants from diverse backgrounds and skill levels come together to showcase their coding prowess. 💫
+👩‍💻This competition challenges participants to solve real-world problems through creative and innovative coding solutions. Whether you're a seasoned developer or just starting your coding journey, Code It On offers a platform to learn, collaborate, and compete in a friendly and supportive environment. 💻 🌐
+Join us for a coding adventure where you can test your skills, network with fellow enthusiasts, and win exciting prizes! 👫✨",
+    dates: "22nd sept",
+    time: "11am Onwards",
+    team_size: "1",
+    prize: "Rs. 30,000",
+    form: "https://forms.gle/pikrCw6WjbuJXDat5",
+    venue: "Online",
     type: 'type',
     status: 'upcoming'
   },
